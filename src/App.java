d8bd3f930fec203d3c710abdde148ad2b58e2d8f
Yuiti Onoue <yuiti.onoue@gmail.com>
2023-10-14 01:52:27 -0300

    read player input

--- src/App.java
+++ src/App.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 import drawers.BoardDrawer;
 import exceptions.ChessException;
+import exceptions.parsemove.ParseMoveException;
 import factory.BoardFactory;
 import models.BoardModel;
+import models.ColorEnum;
+import models.MoveModel;
+import parsers.AlgebricNotationParser;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -15,16 +19,33 @@ public class App {
         try (Scanner in = new Scanner(System.in)) {
             String comando;
 
+            BoardModel board = BoardFactory.createTestBoard();
+            AlgebricNotationParser parser = new AlgebricNotationParser();
+            ColorEnum turnColor = ColorEnum.WHITE;
+            int playCounter = 1;
+            int round = 1;
+
+
             do {
+                drawBoard(board);
+                System.out.println("Turn " + round + ": " + turnColor);
                 comando = in.nextLine();
 
-                if (comando.equals("limpa")) {
-                    limpa();
+                if (comando.equals("clear")) {
+                    clearScreen();
                 } else if (comando.equals("board")) {
-                    board();
-                } else {
-                    System.out.println("Teste");
-                    System.out.println(comando);
+                    drawBoard(board);
+                } else if (!comando.equals("exit")) {
+                    try {
+
+                        MoveModel move = parser.parse(comando, board, turnColor, round);
+                        turnColor = ColorEnum.WHITE.equals(turnColor) ? ColorEnum.BLACK : ColorEnum.WHITE;
+                        playCounter++;
+                        round = (playCounter % 2 == 1) ? round + 1 : round;
+                    } catch (ParseMoveException e) {
+                        System.out.println(e);
+                        comando = in.nextLine();
+                    }
                 }
 
             } while (!comando.equals("exit"));
@@ -34,14 +55,13 @@ public class App {
 
     }
 
-    public static void limpa() {
+    public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
-    public static void board() throws ChessException {
-        BoardModel board = BoardFactory.createTestBoard();
-
+    public static void drawBoard(BoardModel board) throws ChessException {
+        clearScreen();
         System.out.println(BoardDrawer.drawBoard(board));
     }
 }
